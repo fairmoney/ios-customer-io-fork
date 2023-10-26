146abd6fa96efa71e11a79e45fba7309963c9179
Michael Nikolaiev <michael.nikolaiev@fairmoney.io>
2023-10-26 17:25:52 +0200

    Package manifest clean-up.

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         // https://web.archive.org/web/20220525200227/https://www.timc.dev/posts/understanding-swift-packages/
         //
         // Update to exact version until wrapper SDKs become part of testing pipeline.
-        .package(name: "Firebase", url: "https://github.com/fairmoney/ios-customer-io-fork.git", .exact("0.0.4"))
+        .package(name: "Firebase", url: "https://github.com/fairmoney/ios-customer-io-fork.git", .exact("0.0.5"))
     ],
     targets: [ 
         // Common - Code used by multiple modules in the SDK project.
